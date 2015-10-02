cbc697d29ee9682721a5e506645bd92bcf2101f4
Chris Kong <christopher.j.kong@gmail.com>
2015-10-02 11:36:40 +0800

    Added ImagePicker, Save function to AddNewRestaurantVC

--- Chi-Le-Ma/AddRestaurantViewController.swift
+++ Chi-Le-Ma/AddRestaurantViewController.swift
@@ -8,14 +8,106 @@
 
 import UIKit
 
-class AddRestaurantViewController: UITableViewController
+class AddRestaurantViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate
 {
-
+  @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var nameTextField: UITextField!
+  @IBOutlet weak var typeTextField: UITextField!
+  @IBOutlet weak var locationTextField: UITextField!
+  @IBOutlet weak var yesButton: UIButton!
+  @IBOutlet weak var noButton: UIButton!
+  
+  var isVisited = true
+  
   override func viewDidLoad() {
     super.viewDidLoad()
 
   }
+  
+  // MARK: - Table view data source
+  
+  override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
+  {
+    if indexPath.row == 0
+    {
+      if UIImagePickerController.isSourceTypeAvailable(.PhotoLibrary)
+      {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .PhotoLibrary
+        
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+      }
+    }
+    tableView.deselectRowAtIndexPath(indexPath, animated: true)
+  }
+  
+  func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject])
+  {
+    imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+    imageView.contentMode = UIViewContentMode.ScaleAspectFill
+    imageView.clipsToBounds = true
+    
+    let leadingConstraint = NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: imageView.superview, attribute: NSLayoutAttribute.Leading, multiplier: 1, constant: 0)
+    leadingConstraint.active = true
+    
+    let trailingConstraint = NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: imageView.superview, attribute: NSLayoutAttribute.Trailing, multiplier: 1, constant: 0)
+    trailingConstraint.active = true
+    
+    let topConstraint = NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: imageView.superview, attribute: NSLayoutAttribute.Top, multiplier: 1, constant: 0)
+    topConstraint.active = true
+    
+    let bottomConstraint = NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: imageView.superview, attribute: NSLayoutAttribute.Bottom, multiplier: 1, constant: 0)
+    bottomConstraint.active = true
 
-
+    dismissViewControllerAnimated(true, completion: nil)
+  }
+  
+  // MARK: - Action methods
+  
+  @IBAction func saveAction(sender: UIBarButtonItem)
+  {
+    let name = nameTextField.text
+    let type = typeTextField.text
+    let location = locationTextField.text
+    
+    // Validate input fields
+    if name == "" || type == "" || location == ""
+    {
+      let alertController = UIAlertController(title: "Oops", message: "We can't proceed because one of the fields is blank. Please note that all fields are required.", preferredStyle: UIAlertControllerStyle.Alert)
+      alertController.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+      self.presentViewController(alertController, animated: true, completion: nil)
+      return
+    }
+    // Print input data to console
+    print("Name: \(name)")
+    print("Type: \(type)")
+    print("Locaton: \(location)")
+    print("Have you Been here: \(isVisited)")
+    
+    // Dismiss the view controller
+    dismissViewControllerAnimated(true, completion: nil)
+  }
+  
+  @IBAction func toggleBeenHereButton(sender: UIButton)
+  {
+    // Yes button clicked
+    if sender == yesButton
+    {
+      isVisited = true
+      yesButton.backgroundColor = UIColor(red: 235.0/255.0, green: 73.0/255.0, blue: 27.0/255.0, alpha: 1.0)
+      noButton.backgroundColor = UIColor.grayColor()
+      
+    } else if sender == noButton
+    {
+      isVisited = false
+      yesButton.backgroundColor = UIColor.grayColor()
+      noButton.backgroundColor = UIColor(red: 235.0/255.0, green: 73.0/255.0, blue: 27.0/255.0, alpha: 1.0)
+    }
+  }
+  
+  
 
 }
+
--- Chi-Le-Ma/RestaurantTableViewController.swift
+++ Chi-Le-Ma/RestaurantTableViewController.swift
@@ -170,4 +170,5 @@ class RestaurantTableViewController: UITableViewController
   
   
   
+  
 }
